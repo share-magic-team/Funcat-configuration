182811900535e98a81ff9dcb9ea110d016218add
Новгородцев Антон <anton.novgorodcev@gmail.com>
2020-08-31 23:34:59 +0300

    Abstractions for DI added

---
+++ FuncatConfiguration.Abstractions/FuncatConfiguration.Abstractions/IServiceCollectionRegistrar.cs
@@ -0,0 +1,15 @@
+﻿namespace FuncatConfiguration.Abstractions
+{
+    /// <summary>
+    /// Allows to register configurations in DI container
+    /// </summary>
+    public interface IServiceCollectionRegistrar
+    {
+        /// <summary>
+        /// Register type in DI container
+        /// </summary>
+        /// <typeparam name="T">Configuration type</typeparam>
+        /// <param name="config">Configuration</param>
+        void Register<T>(T config);
+    }
+}
