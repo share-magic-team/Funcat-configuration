3406d41867f1ec88da9da347f0b961b41a4590cc
Новгородцев Антон <anton.novgorodcev@gmail.com>
2020-09-01 23:22:18 +0300

    FuncatConfiguration.DI.MicrosoftDependencyInjection updated

--- FuncatConfiguration.DI.MicrosoftDependencyInjection/FuncatConfiguration.DI.MicrosoftDependencyInjection/ConfigurationManagerBuilderExtensions.cs
+++ FuncatConfiguration.DI.MicrosoftDependencyInjection/FuncatConfiguration.DI.MicrosoftDependencyInjection/ConfigurationManagerBuilderExtensions.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FuncatConfiguration.DI.MicrosoftDependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Use Microsoft dependency injection as DI container for configurations
+        /// Register Funcat configuration types in Microsoft DI
         /// </summary>
-        /// <param name="builder"></param>
         /// <param name="serviceCollection">Service collection for services registration</param>
-        /// <returns>Builder</returns>
+        /// <param name="configurationManager">Funcat configuration manager</param>
+        /// <returns>Service collection</returns>
         public static IServiceCollection RegisterConfigurationTypes(this IServiceCollection serviceCollection, ConfigurationManager configurationManager)
         {
-            // Some actions
+            foreach (var info in configurationManager.GetConfigurationTypeInfos())
+            {
+                if (info.RegisterInServiceCollection)
+                    serviceCollection.AddTransient(info.Type, (_) => configurationManager.GetConfigurationAsync(info.Name, info.Type, CancellationToken.None).Result);
+            }
             return serviceCollection;
         }
     }
