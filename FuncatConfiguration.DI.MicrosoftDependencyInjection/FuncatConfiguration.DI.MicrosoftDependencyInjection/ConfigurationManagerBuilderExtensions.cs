83c30658bb2a444ead3b62f4d569751ada89cfab
Новгородцев Антон <anton.novgorodcev@gmail.com>
2020-09-01 18:34:25 +0300

    Microsoft DI registrar added

---
+++ FuncatConfiguration.DI.MicrosoftDependencyInjection/FuncatConfiguration.DI.MicrosoftDependencyInjection/ConfigurationManagerBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FuncatConfiguration.DI.MicrosoftDependencyInjection
+{
+    public static class ConfigurationManagerBuilderExtensions
+    {
+        /// <summary>
+        /// Use Microsoft dependency injection as DI container for configurations
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="serviceCollection">Service collection for services registration</param>
+        /// <returns>Builder</returns>
+        public static ConfigurationManagerBuilder WithMicrosoftDI(this ConfigurationManagerBuilder builder, IServiceCollection serviceCollection)
+        {
+            return builder.WithServiceCollectionRegistrar(new MicrosoftDIRegistrar(serviceCollection));
+        }
+    }
+}
